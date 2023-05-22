90843b79a1bc5b0453c16fafcd46887a7c08627a
mjpurin <mjpurin@gmail.com>
2023-05-22 11:48:53 +0900

    ExMethod4にコメント追加

--- ExMethod4.java
+++ ExMethod4.java
@@ -9,6 +9,12 @@ public class ExMethod4{
 		System.out.printf("税込み価格は%d円です%n",taxPrice);
 
 	}
+	/*
+	 * calcPriceメソッド
+	 * 税抜き価格と、消費税率から税込み価格を求める
+	 * 引数:price(int)税抜き価格,taxRatio(int)税率
+	 * 戻り値:税込み価格(int)
+	 */
 	public static int calcPrice(int price,int taxRatio){
 		price = (int)(price * (1+taxRatio/100.0));
 		return price;
