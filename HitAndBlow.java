d9c7abf7a54605f8369ab6f00dba2573537bb8d6
mjpurin <mjpurin@gmail.com>
2023-06-02 12:58:51 +0900

    HitAndBlow改良

--- HitAndBlow.java
+++ HitAndBlow.java
@@ -3,15 +3,13 @@ public class HitAndBlow{
 	public static void main(String[] args){
 		final int NUM_COUNT=4;
 		int[] nums={1,2,3,4,5,6,7,8,9};
-		int[] ans=new int[NUM_COUNT];
 		for(int i=0;i<NUM_COUNT;i++){
 			int index = new Random().nextInt(nums.length-i)+i;
 			int temp = nums[index];
 			nums[index]=nums[i];
 			nums[i] = temp;
-			ans[i]=nums[i];
 		}
-		//System.out.println(Arrays.toString(ans));
+		System.out.println(Arrays.toString(nums));
 		int count =0,hit,blow;
 		do{
 			count++;
@@ -19,16 +17,16 @@ public class HitAndBlow{
 			System.out.printf("数字を%d桁で入力してください>",NUM_COUNT);
 			int input = new Scanner(System.in).nextInt();
 			int[] userAns = new int[NUM_COUNT];
-			for(int i=ans.length-1;i>=0;i--){
+			for(int i=userAns.length-1;i>=0;i--){
 				userAns[i]=input % 10;
 				input /= 10;
 			}
 			for(int i=0;i<NUM_COUNT;i++){
-				if(userAns[i] == ans[i]){
+				if(userAns[i] == nums[i]){
 					hit++;
 				}else{
 					for(int j=0;j<NUM_COUNT;j++){
-						if(userAns[i] == ans[j]){
+						if(userAns[i] == nums[j]){
 							blow++;
 						}
 					}
